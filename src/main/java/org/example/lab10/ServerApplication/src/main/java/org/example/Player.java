27cdf439bf6eeb587e3f140cd755729ad8302a9c
Ionela Alexandra <63660313+ionero12@users.noreply.github.com>
2023-05-13 12:45:14 +0300

    lab 10 compulsory + optional

---
+++ src/main/java/org/example/lab10/ServerApplication/src/main/java/org/example/Player.java
@@ -0,0 +1,17 @@
+package org.example.lab10.ServerApplication.src.main.java.org.example;
+
+public class Player {
+    private String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
